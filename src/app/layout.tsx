e3ccdf1fc84beeed0ca43069b172c794cf2a684a
Matiu <mateovranych@hotmail.com>
2024-05-15 20:11:06 -0300

    inicio de la app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import SessionAuthProvider from '@/context/SessionAuthProvider';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className='container'>
+          <SessionAuthProvider>{children}</SessionAuthProvider>
+        </main>
+        </body>
     </html>
   );
 }
---
+++ src/context/SessionAuthProvider.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { SessionProvider } from "next-auth/react"
+
+
+
+interface Props{
+    children: React.ReactNode
+}
+
+const SessionAuthProvider = ({children}: Props) => {
+  return (
+    <SessionProvider>{children}</SessionProvider>
+  )
+}
+
+export default SessionAuthProvider
+
+
